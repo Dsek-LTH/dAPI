a2736565455fcd9580b6d7c906f2281c6560fd22
Emil Hammarström <emil.a.hammarstrom@gmail.com>
2017-07-03 04:45:50 +0200

    app.js formatting

--- app.js
+++ app.js
@@ -17,80 +17,76 @@ var app = express();
 var port = 3000;
 
 if (cluster.isMaster) {
-	// Count the machine's CPUs
-	var cpuCount = require('os').cpus().length;
+  // Count the machine's CPUs
+  var cpuCount = require('os').cpus().length;
 
-	// Create a worker for each CPU
-	for (var i = 0; i < cpuCount; i += 1) {
-		cluster.fork();
-	}
-	//Listen for dying workers
-	cluster.on('exit', function (worker) {
-		// Replace the dead worker
-		cluster.fork();
+  // Create a worker for each CPU
+  for (var i = 0; i < cpuCount; i += 1) {
+    cluster.fork();
+  }
 
-	});
-
+  //Listen for dying workers
+  cluster.on('exit', function (worker) {
+    // Replace the dead worker
+    cluster.fork();
+  });
 } else {
+  // view engine setup
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'jade');
 
-	// view engine setup
-	app.set('views', path.join(__dirname, 'views'));
-	app.set('view engine', 'jade');
+  // uncomment after placing your favicon in /public
+  //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+  app.use(logger('dev'));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(cookieParser());
+  app.use(express.static(path.join(__dirname, 'public')));
 
-	// uncomment after placing your favicon in /public
-	//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-	app.use(logger('dev'));
-	app.use(bodyParser.json());
-	app.use(bodyParser.urlencoded({ extended: false }));
-	app.use(cookieParser());
-	app.use(express.static(path.join(__dirname, 'public')));
+  app.use('/', index);
+  app.use('/users', users);
 
-	app.use('/', index);
-	app.use('/users', users);
+  // catch 404 and forward to error handler
+  app.use(function(req, res, next){
+    res.status(404);
+    // respond with html page
+    if (req.accepts('html')) {
+      res.render('404', { url: req.url });
+      return;
+    }
 
-	// catch 404 and forward to error handler
-	app.use(function(req, res, next){
-		res.status(404);
-		// respond with html page
-		if (req.accepts('html')) {
-			res.render('404', { url: req.url });
-			return;
-		}
+    // respond with json
+    if (req.accepts('json')) {
+      res.send({ error: 'Not found' });
+      return;
+    }
 
-		// respond with json
-		if (req.accepts('json')) {
-			res.send({ error: 'Not found' });
-			return;
-		}
+    // default to plain-text. send()
+    res.type('txt').send('Not found');
+  });
 
-		// default to plain-text. send()
-		res.type('txt').send('Not found');
-	});
+  app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  });
 
-	app.use(function(req, res, next) {
-		var err = new Error('Not Found');
-		err.status = 404;
-		next(err);
-	});
-	// error handler
-	app.use(function(err, req, res, next) {
-		// set locals, only providing error in development
-		res.locals.message = err.message;
-		res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // error handler
+  app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-		// render the error page
-		res.status(err.status || 500);
-		res.render('error');
-	});
-	console.log('Application running!');
-	/**
-	 * Create HTTP server.
-	 */
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
 
-	var server = http.createServer(app);
-	/**
-	 * Listen on provided port, on all network interfaces.
-	 */
+  console.log('Application running!');
 
-	server.listen(port);
+  // create HTTP-server
+  var server = http.createServer(app);
+
+  // listen to port
+  server.listen(port);
 }
