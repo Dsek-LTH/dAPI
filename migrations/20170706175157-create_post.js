2d94de6d08281b0aea64cddf4dfe8c5149f8b2bf
Mattemagikern <mansgariusson@gmail.com>
2017-07-06 20:24:49 +0200

    Created Post table

---
+++ migrations/20170706175157-create_post.js
@@ -0,0 +1,41 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    queryInterface.createTable(
+      'posts',
+      {
+        id: {
+          type: Sequelize.INTEGER,
+          primaryKey: true,
+          autoIncrement: true
+        },
+        createdAt: {
+          type: Sequelize.DATE
+        },
+        updatedAt: {
+          type: Sequelize.DATE
+        },
+        creator: {
+          type: Sequelize.INTEGER,
+          references: {
+            model: 'users',
+            key: 'id'
+          },
+          onUpdate: 'cascade',
+          onDelete: 'cascade'
+        },
+        header: {
+          type: Sequelize.STRING,
+          allowNull: false
+        },
+        text: {
+          type: Sequelize.STRING,
+          allowNull: false
+        }
+      })
+  },
+  down: function (queryInterface, Sequelize) {
+    queryInterface.dropTable('posts');
+  }
+};
